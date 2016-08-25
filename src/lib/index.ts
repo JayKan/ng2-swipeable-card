517e0c658b029b6d7ee9d376f3658953061bef12
Jay Kan <leicasper@gmail.com>
2016-08-25 09:34:26 -0700

    chore: expose swipeable-card as a module

---
+++ src/lib/index.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+import { SWIPEABLE_CARD_DIRECTIVES } from './swipeable-card';
+
+export class HammerConfig extends HammerGestureConfig {
+  overrides = <any> {
+    'swipe': { velocity: 0.4, threshold: 20 } // override default settings
+  }
+}
+
+@NgModule({
+  declarations: [ SWIPEABLE_CARD_DIRECTIVES ],
+  exports: [ SWIPEABLE_CARD_DIRECTIVES ],
+  providers: [
+    { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig }
+  ]
+})
+export class SwipeableCardModule { }
